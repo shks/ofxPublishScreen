3ff2e1e54b87fd3589d04691b8cd25bec4808d10
James Kong <fishkingsin@gmail.com>
2012-12-14 11:51:45 +0800

    added ios example

---
+++ example-ios-receiver/src/testApp.mm
@@ -0,0 +1,76 @@
+#include "testApp.h"
+#include "ofxPublishScreen.h"
+
+ofxPublishScreen::Subscriber subs;
+//--------------------------------------------------------------
+void testApp::setup(){	
+	// initialize the accelerometer
+	ofxAccelerometer.setup();
+	
+	//If you want a landscape oreintation 
+	//iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
+	
+	ofBackground(127,127,127);
+	subs.setup("192.168.1.7", 20000);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	subs.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	subs.draw();
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchUp(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::deviceOrientationChanged(int newOrientation){
+
+}
+
---
+++ example-ios-sender/src/testApp.mm
@@ -0,0 +1,77 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){	
+	// initialize the accelerometer
+	ofxAccelerometer.setup();
+	
+	//If you want a landscape oreintation 
+	//iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
+	
+	ofBackground(127,127,127);
+	pub.setup(20000, 1024, 768);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	pub.begin();
+	ofClear(ofRandom(255), ofRandom(255), ofRandom(255));
+	//	ofCircle(ofGetMouseX(), ofGetMouseY(), 100);
+	pub.end();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	pub.draw();
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchUp(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::deviceOrientationChanged(int newOrientation){
+
+}
+
